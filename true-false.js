4a1ebb9b7669aefe75e4b15906ea726812a2d519
koushik16101085 <16101085@uap-bd.edu>
2021-02-03 02:51:13 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,25 @@
+//falsy
+//0
+//undefined
+//null
+//NuN
+//false
+//Truthy
+//'0',' ',[]
+
+
+//const age = 4;
+//const name = "Solaiman";
+//let name = null;
+//let name = false;
+//let name = 'false';
+let name = 'false';
+
+console.log(name);
+if (name || name == 0) {
+    console.log("condition is true");
+    
+}
+else{
+    console.log("condition is false");
+}
