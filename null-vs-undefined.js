d7ee3a784f991aa75d8f0439f955c168b7848923
koushik16101085 <16101085@uap-bd.edu>
2021-02-03 03:26:52 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+//let pakhi;
+// function add(num1,num2) {
+//     console.log(num1 + num2);
+//     return
+    
+// }
+
+// function add(num1,num2){
+
+//     console.log(num2);
+// }
+// const result = add(13);
+ //const result = add(13 , 82);
+//  const premik ={name:"Smart dude",phone:458421};
+// console.log(premik.gf);
+//console.log(pakhi);
+
+// let fun = undefined;
+// console.log(fun);
+
+// let ages = [2, 5, 9]
+// console.log(ages[11]);
