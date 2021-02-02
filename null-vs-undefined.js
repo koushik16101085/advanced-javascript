4f600bbcf29c6768525c2ea562fe1e4dd1a78623
koushik16101085 <16101085@uap-bd.edu>
2021-02-03 03:36:28 +0600

    nil-vs-undefined-interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,22 +1,36 @@
-let pakhi;
-function add(num1,num2) {
-    console.log(num1 + num2);
-    return
+// let pakhi;
+// function add(num1,num2) {
+//     console.log(num1 + num2);
+//     return
     
+// }
+
+// function add(num1,num2){
+
+//     console.log(num2);
+// }
+// const result = add(13);
+//  const result = add(13 , 82);
+//  const premik ={name:"Smart dude",phone:458421};
+// console.log(premik.gf);
+// console.log(pakhi);
+
+// let fun = undefined;
+// console.log(fun);
+
+// let ages = [2, 5, 9]
+// console.log(ages[11]);
+
+//quiz let
+//What would be the output from the function below?
+
+
+
+function doSomething(x, y){
+
+console.log(y);
+
 }
 
-function add(num1,num2){
-
-    console.log(num2);
-}
-const result = add(13);
- const result = add(13 , 82);
- const premik ={name:"Smart dude",phone:458421};
-console.log(premik.gf);
-console.log(pakhi);
-
-let fun = undefined;
-console.log(fun);
-
-let ages = [2, 5, 9]
-console.log(ages[11]);
+const result =doSomething(32);
+console.log(result);
