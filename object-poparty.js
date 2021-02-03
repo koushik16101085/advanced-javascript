e478d7396322cca1ffe9aade8898e454dd8a3664
koushik16101085 <16101085@uap-bd.edu>
2021-02-03 18:35:41 +0600

    onake kichu shiklam

--- object-poparty.js
+++ object-poparty.js
@@ -1,23 +1,29 @@
-const students = [
-    {id:21, name: 'Omar Sunny'},
-    {id: 31, name: 'Maannaaaaaaa'},
-    {id: 41, name: 'Moyouri'},
-    {id: 71, name: 'Deepjol'}
+// const students = [
+//     {id:21, name: 'Omar Sunny'},
+//     {id: 31, name: 'Maannaaaaaaa'},
+//     {id: 41, name: 'Moyouri'},
+//     {id: 71, name: 'Deepjol'}
+// ];
+const names = ['omar sunny','manna','moyouri','deepjol'
+    // { name: 'Omar Sunny'},
+    // { name: 'Maannaaaaaaa'},
+    // { name: 'Moyouri'},
+    // { name: 'Deepjol'}
 ];
 const output=[];
-for (let i = 0; i < students.length; i++) {
-    const element = students[i];
+for (let i = 0; i < names.length; i++) {
+    const element = names[i];
     output.push(element);
     
 }
 console.log(output);
-const names = students.map(s=>s.name)
-console.log(names);
+// const names = students.map(s=>s.name)
+// console.log(names);
 
-const ides = students.map(s=>s.id)
-console.log(ides);
+// const ides = students.map(s=>s.id)
+// console.log(ides);
 
-const bigger = students.filter(s=>s.id>40);
-console.log(bigger);
-const bigger1 = students.find(s=>s.id>40);
-console.log(bigger1);
+// const bigger = students.filter(s=>s.id>40);
+// console.log(bigger);
+// const bigger1 = students.find(s=>s.id>40);
+// console.log(bigger1);
