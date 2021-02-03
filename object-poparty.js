d9d12206428e810f49b2afe170fb3cbda5b4649d
koushik16101085 <16101085@uap-bd.edu>
2021-02-03 17:59:10 +0600

    object-poparty Interview js

--- object-poparty.js
+++ object-poparty.js
@@ -10,14 +10,14 @@ for (let i = 0; i < students.length; i++) {
     output.push(element);
     
 }
-//console.log(output);
-// const names = students.map(s=>s.name)
-// console.log(names);
+console.log(output);
+const names = students.map(s=>s.name)
+console.log(names);
 
-// const ides = students.map(s=>s.id)
-// console.log(ides);
+const ides = students.map(s=>s.id)
+console.log(ides);
 
-// const bigger = students.filter(s=>s.id>40);
-// console.log(bigger);
+const bigger = students.filter(s=>s.id>40);
+console.log(bigger);
 const bigger1 = students.find(s=>s.id>40);
 console.log(bigger1);
